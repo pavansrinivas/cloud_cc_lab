252c3044ec3e9ad09f19955cdb1392cc7a5dd5ce
Pavan Srinivas <pavan030395@gmail.com>
2024-09-17 11:33:34 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+jsx
+import React from 'react';
+function App() {
+return (
+<div className="App">
+  <header className="App  header">
+   <h1>Welcome to My React App</h1>
+  </header>
+</div>
+  );
+    }
+ export default App;
